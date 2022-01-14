2e8affe42af7fd96d049c46d9ccf6f50fd39cae4
Maxim <githubmax@ukr.net>
2022-01-14 20:22:03 +0200

    В файле WebSecurityConfig.java добавил аннотацию @Lazy над бином getPasswordEncoder(). Пытаюсь избавиться от циклической ссылки возникшей при запуске в Heroku.

--- src/main/java/com/example/demo2/config/WebSecurityConfig.java
+++ src/main/java/com/example/demo2/config/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package com.example.demo2.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -31,6 +32,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     private DataSource dataSource;
 
     //Добавляем кодирование паролей
+    @Lazy //пытаюсь избавиться от циклической ссылки возникшей при запуске в Heroku
     @Bean
     public PasswordEncoder getPasswordEncoder(){
         return new  BCryptPasswordEncoder(8);
